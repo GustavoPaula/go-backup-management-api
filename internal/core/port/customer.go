559d9314cc3d4403a7b86acbb9ef9fb124114f52
GustavoPaula <gustavosilvadepaula95@outlook.com>
2025-08-25 19:56:34 -0300

    feat: Add Customer Service

--- internal/core/port/customer.go
+++ internal/core/port/customer.go
@@ -9,6 +9,7 @@ import (
 type CustomerRepository interface {
 	CreateCustomer(ctx context.Context, customer *domain.Customer) (*domain.Customer, error)
 	GetCustomerByID(ctx context.Context, id string) (*domain.Customer, error)
+	GetCustomerByName(ctx context.Context, name string) (*domain.Customer, error)
 	ListCustomers(ctx context.Context, page, limit int64) ([]domain.Customer, error)
 	UpdateCustomer(ctx context.Context, customer *domain.Customer) (*domain.Customer, error)
 	DeleteCustomer(ctx context.Context, id string) error
---
+++ internal/core/service/customer.go
@@ -0,0 +1,94 @@
+package service
+
+import (
+	"context"
+
+	"github.com/GustavoPaula/go-backup-management-api/internal/core/domain"
+	"github.com/GustavoPaula/go-backup-management-api/internal/core/port"
+)
+
+type CustomerService struct {
+	repo port.CustomerRepository
+}
+
+func NewCustomerService(repo port.CustomerRepository) *CustomerService {
+	return &CustomerService{
+		repo,
+	}
+}
+
+func (cs *CustomerService) CreateCustomer(ctx context.Context, customer *domain.Customer) (*domain.Customer, error) {
+	existingCustomer, err := cs.repo.GetCustomerByName(ctx, customer.Name)
+	if err != nil {
+		return nil, domain.ErrInternal
+	}
+
+	if existingCustomer != nil {
+		return nil, domain.ErrConflictingData
+	}
+
+	customer, err = cs.repo.CreateCustomer(ctx, customer)
+	if err != nil {
+		return nil, domain.ErrInternal
+	}
+
+	return customer, nil
+}
+
+func (cs *CustomerService) GetCustomerByID(ctx context.Context, id string) (*domain.Customer, error) {
+	var customer *domain.Customer
+
+	customer, err := cs.repo.GetCustomerByID(ctx, id)
+	if err != nil {
+		return nil, domain.ErrInternal
+	}
+
+	return customer, nil
+}
+
+func (cs *CustomerService) ListCustomers(ctx context.Context, page, limit int64) ([]domain.Customer, error) {
+	var customers []domain.Customer
+
+	customers, err := cs.repo.ListCustomers(ctx, page, limit)
+	if err != nil {
+		return nil, domain.ErrInternal
+	}
+
+	return customers, nil
+}
+
+func (cs *CustomerService) UpdateCustomer(ctx context.Context, customer *domain.Customer) (*domain.Customer, error) {
+	existingCustomer, err := cs.repo.GetCustomerByID(ctx, customer.ID)
+	if err != nil {
+		return nil, domain.ErrInternal
+	}
+
+	if customer.Name == "" {
+		customer.Name = existingCustomer.Name
+	}
+
+	updateCustomer, err := cs.repo.UpdateCustomer(ctx, customer)
+	if err != nil {
+		return nil, domain.ErrInternal
+	}
+
+	return updateCustomer, nil
+}
+
+func (cs *CustomerService) DeleteCustomer(ctx context.Context, id string) error {
+	existingCustomer, err := cs.repo.GetCustomerByID(ctx, id)
+	if err != nil {
+		return domain.ErrInternal
+	}
+
+	if existingCustomer != nil {
+		return domain.ErrDataNotFound
+	}
+
+	err = cs.repo.DeleteCustomer(ctx, id)
+	if err != nil {
+		return domain.ErrInternal
+	}
+
+	return nil
+}
