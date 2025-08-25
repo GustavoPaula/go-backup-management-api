14a0653bd2133fa2dcd03a43013c0833e7ffb16a
GustavoPaula <gustavosilvadepaula95@outlook.com>
2025-08-25 16:41:23 -0300

    feat: Add port customer

---
+++ internal/core/port/customer.go
@@ -0,0 +1,23 @@
+package port
+
+import (
+	"context"
+
+	"github.com/GustavoPaula/go-backup-management-api/internal/core/domain"
+)
+
+type CustomerRepository interface {
+	CreateCustomer(ctx context.Context, customer *domain.Customer) (*domain.Customer, error)
+	GetCustomerByID(ctx context.Context, id string) (*domain.Customer, error)
+	ListCustomers(ctx context.Context, page, limit int64) ([]domain.Customer, error)
+	UpdateCustomer(ctx context.Context, customer *domain.Customer) (*domain.Customer, error)
+	DeleteCustomer(ctx context.Context, id string) error
+}
+
+type CustomerService interface {
+	CreateCustomer(ctx context.Context, customer *domain.Customer) (*domain.Customer, error)
+	GetCustomerByID(ctx context.Context, id string) (*domain.Customer, error)
+	ListCustomers(ctx context.Context, page, limit int64) ([]domain.Customer, error)
+	UpdateCustomer(ctx context.Context, customer *domain.Customer) (*domain.Customer, error)
+	DeleteCustomer(ctx context.Context, id string) error
+}
