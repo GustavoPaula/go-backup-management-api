34fbdaab297f17bfce76cbd2288683319306bb06
gustavo <gustavo@aswti.com.br>
2025-09-24 16:23:38 -0300

    refactor: ajustes migration devices

--- internal/adapter/storage/postgres/migrations/000003_create_devices_table.up.sql
+++ internal/adapter/storage/postgres/migrations/000003_create_devices_table.up.sql
@@ -1,13 +1,15 @@
--- CreateTable
 CREATE TABLE "devices" (
-    "id" uuid  PRIMARY KEY NOT NULL  DEFAULT gen_random_uuid(),
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
-    "customer_id" uuid NOT NULL DEFAULT gen_random_uuid(),
-    "created_at" timestamptz NOT NULL DEFAULT (now()),
-    "updated_at" timestamptz NOT NULL DEFAULT (now()),
-
-    CONSTRAINT "devices_pkey" PRIMARY KEY ("id")
+    "customer_id" uuid NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT NOW(),
+    "updated_at" timestamptz NOT NULL DEFAULT NOW()
 );
 
--- AddForeignKey
-ALTER TABLE "devices" ADD CONSTRAINT "devices_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- Foreign Key
+ALTER TABLE "devices" ADD CONSTRAINT "devices_customer_id_fkey" 
+FOREIGN KEY ("customer_id") REFERENCES "customers"("id") 
+ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- Índices para performance
+CREATE INDEX "idx_devices_customer_id" ON "devices"("customer_id");
