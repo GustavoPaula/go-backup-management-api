894e1eb56d56b866f683b2cf136f7c0729ee3356
gustavo <gustavo@aswti.com.br>
2025-10-06 16:00:17 -0300

    feat: add backup plan handler

--- internal/adapter/http/handler/backup_plan.go
+++ internal/adapter/http/handler/backup_plan.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
-	"context"
+	"encoding/json"
+	"net/http"
+	"time"
 
+	"github.com/GustavoPaula/go-backup-management-api/internal/adapter/http/response"
 	"github.com/GustavoPaula/go-backup-management-api/internal/core/domain"
 	"github.com/GustavoPaula/go-backup-management-api/internal/core/port"
 	"github.com/google/uuid"
@@ -12,28 +15,72 @@ type BackupPlanHandler struct {
 	svc port.BackupPlanService
 }
 
+type backupPlanRequest struct {
+	Name            string              `json:"name"`
+	BackupSizeBytes int                 `json:"backup_size_bytes"`
+	DeviceID        uuid.UUID           `json:"device_id"`
+	WeekDay         []backupPlanWeekDay `json:"week_day"`
+}
+
+type backupPlanWeekDay struct {
+	Day     string    `json:"day"`
+	TimeDay time.Time `json:"time_day"`
+}
+
 func NewBackupPlanHandler(svc port.BackupPlanService) *BackupPlanHandler {
 	return &BackupPlanHandler{
 		svc,
 	}
 }
 
-func CreateBackupPlan(ctx context.Context, backupPlan *domain.BackupPlan) (*domain.BackupPlan, error) {
-	return nil, nil
+func (bph *BackupPlanHandler) CreateBackupPlan(w http.ResponseWriter, r *http.Request) {
+	var req backupPlanRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		response.JSON(w, http.StatusBadRequest, "JSON inválido", nil, nil)
+		return
+	}
+	defer r.Body.Close()
+
+	backupPlan := &domain.BackupPlan{
+		Name:            req.Name,
+		BackupSizeBytes: req.BackupSizeBytes,
+		DeviceID:        req.DeviceID,
+	}
+
+	backupPlan.WeekDay = make([]domain.BackupPlanWeekDay, len(req.WeekDay))
+	for i, wdReq := range req.WeekDay {
+		backupPlan.WeekDay[i] = domain.BackupPlanWeekDay{
+			Day:     wdReq.Day,
+			TimeDay: wdReq.TimeDay,
+		}
+	}
+
+	newBackupPlan, err := bph.svc.CreateBackupPlan(r.Context(), backupPlan)
+	if err != nil {
+		if err == domain.ErrDataNotFound {
+			response.JSON(w, http.StatusNotFound, "Erro ao criar plano de backup", nil, err.Error())
+			return
+		}
+
+		response.JSON(w, http.StatusInternalServerError, "Erro ao criar plano de backup", nil, nil)
+		return
+	}
+
+	response.JSON(w, http.StatusCreated, "Plano de backup criado com sucesso", newBackupPlan, nil)
 }
 
-func GetBackupPlan(ctx context.Context, id uuid.UUID) (*domain.BackupPlan, error) {
-	return nil, nil
+func GetBackupPlan(w http.ResponseWriter, r *http.Request) {
+
 }
 
-func ListBackupPlans(ctx context.Context, page, limit int) ([]domain.BackupPlan, error) {
-	return nil, nil
+func ListBackupPlans(w http.ResponseWriter, r *http.Request) {
+
 }
 
-func UpdateBackupPlan(ctx context.Context, backupPlan *domain.BackupPlan) (*domain.BackupPlan, error) {
-	return nil, nil
+func UpdateBackupPlan(w http.ResponseWriter, r *http.Request) {
+
 }
 
-func DeleteBackupPlan(ctx context.Context, id uuid.UUID) error {
-	return nil
+func DeleteBackupPlan(w http.ResponseWriter, r *http.Request) {
+
 }
